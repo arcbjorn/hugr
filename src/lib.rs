ec7b538757d05eca6e5c64bf2b6591543fa5a720
arcbjorn <arky.happ@gmail.com>
2026-06-26 18:03:40 -0300

    feat(lib): add library entrypoint

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+mod cli;
+mod commands;
+mod store;
+
+use cli::Command;
+
+pub async fn run(args: Vec<String>) -> Result<(), String> {
+    let command = Command::parse(&args)?;
+    commands::execute(command).await
+}
